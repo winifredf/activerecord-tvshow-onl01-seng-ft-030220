f45a45e555b5a7143f647de1045d19d8c12cdb2a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-06 03:54:23 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,32 @@
+class Show < ActiveRecord::Base
+  
+  def Show::highest_rating
+    Show.maximum('rating')
+  end
+  
+  def Show::most_popular_show
+    Show.order('rating DESC').first
+  end
+  
+  def Show::lowest_rating
+    Show.minimum('rating')
+  end
+  
+  def Show::least_popular_show
+    Show.order('rating ASC').first
+  end
+  
+  def Show::ratings_sum
+    Show.sum('rating')
+  end
+  
+  def Show::popular_shows
+    Show.where('rating > 5')
+  end
+  
+  def Show::shows_by_alphabetical_order
+    Show.order('name ASC')
+  end
+  
+  
+end
